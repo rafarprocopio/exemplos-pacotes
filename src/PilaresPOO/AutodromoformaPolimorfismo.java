a0a5b042993fc415d824cd7e09079c5f9afc953b
Rafaely RP <rafarprocopio@gmail.com>
2024-06-21 14:50:08 -0300

    Alterações exemplos Pilares POO

--- src/PilaresPOO/AutodromoformaPolimorfismo.java
+++ src/PilaresPOO/AutodromoformaPolimorfismo.java
@@ -4,14 +4,14 @@ public class AutodromoformaPolimorfismo {
     public static void main(String[] args) {
         CarroformaPolimorfismo jeep = new CarroformaPolimorfismo();
         jeep.setChassi("546484");
-        jeep.ligar();
+        //jeep.ligar();
 
         MotoformaPolimorfismo Z400 = new MotoformaPolimorfismo();
         Z400.setChassi("45668");
         //Z400.ligar();
 
-        //VeiculoformaPolimorfismo coringa = jeep;
-        //coringa.ligar(); 
+        VeiculoformaPolimorfismo coringa = jeep;
+        coringa.ligar(); 
 
     
 
