176bf985454b56dc9190f8a1e822849205227983
Rafaely RP <rafarprocopio@gmail.com>
2024-06-22 15:22:33 -0300

    Alterações execicio pratico de  Pilar POO

--- src/PraticaPilaresPOO/ComputadorPedrinho.java
+++ src/PraticaPilaresPOO/ComputadorPedrinho.java
@@ -29,15 +29,16 @@ public class ComputadorPedrinho {
 		    NÃO SE SABE QUAL APP 
 		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
 		 */
-		String appEscolhido="tlg"; 
+		String appEscolhido="fcb"; 
 		
-		if(appEscolhido.equals("msn"))
+		if(appEscolhido.equals("msn")) {
 			smi = new MSNMessenger();
-		else if(appEscolhido.equals("fbm"))
+		} else if(appEscolhido.equals("fcb")) {
 			smi = new FacebookMessenger();
-		else if(appEscolhido.equals("tlg"))
+		} else if(appEscolhido.equals("tlg")) {
 			smi = new Telegram();
-		
+		}		
+			
 			
 		smi.enviarMensagem();
 		smi.receberMensagem();        
